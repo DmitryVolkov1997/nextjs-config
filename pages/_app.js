bf92abd455c724465a6ea3ea2edd707988533895
Dmitriy <dmitrij_volkov_2014@mail.ru>
2022-07-16 09:19:48 +0600

    added nextjs-config

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import '../app/components/assets/styles/globals.scss'
+import Layout from '../app/components/layout/Layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
